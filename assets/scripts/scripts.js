97264ec461aee1cd0fd4f620c65291243aace2f5
marzec <gregmarzec02@gmail.com>
2022-07-06 23:21:20 -0400

    added url function

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,27 +1,8 @@
 var key = "b947ea02345fcb633ceba05df9ae4ea8";
 var searchButton = $("#searchButton")
 var searchInput = $("#searchInput")
+
 let input;
-
-
-$("#searchButton").click(function(event) {
-    event.preventDefault();
-    input = searchInput.val();
-    console.log(input);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 var update = function() {
@@ -29,3 +10,39 @@ var update = function() {
     time.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }; 
 setInterval(update, 1000);
+
+$("#searchButton").click(function(event) {
+    event.preventDefault();
+    input = searchInput.val();
+    console.log(input);
+    url();
+})
+
+function url() {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + ",USA&units=imperial&appid=" + key
+    console.log(url);
+    fetch(url)
+        .then(function (response) {
+            if (!response.ok) {
+                console.log("WRONG");
+                return
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data.coord.lat);
+            console.log(data.coord.lon)
+        })
+}
+
+
+
+
+
+
+
+
+
+
+
+
