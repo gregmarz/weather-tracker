3ca8e9332b10f6552b5caf1d0b6b98de993add15
marzec <gregmarzec02@gmail.com>
2022-07-07 01:21:57 -0400

    added city search function

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,9 +3,21 @@ var searchButton = $("#searchButton");
 var searchInput = $("#searchInput");
 var cityList = document.getElementById("cityList");
 
-var input;
+var cityName = document.getElementById("cityName");
+var weatherImg = document.getElementById("weatherImg")
+var cityTemp = document.getElementById("cityTemp");
+var cityHumid = document.getElementById("cityHumid");
+var cityWind = document.getElementById("cityWind");
+var cityUV = document.getElementById("cityUV");
+
+var input = [];
 var latitude = 0;
 var longitude = 0;
+
+var temp = 0;
+var humidity = 0;
+var wind_speed = 0;
+var uvi = 0;
 
 var update = function() {
     var time = document.getElementById("datetime")
@@ -29,9 +41,8 @@ function addCity() {
     cityNewCont.appendChild(cityNew);
 }
 
-
 function requestOne() {
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + ",USA&units=imperial&appid=" + key
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=imperial&appid=" + key
     console.log(url);
     fetch(url)
         .then(function (response) {
@@ -42,20 +53,34 @@ function requestOne() {
             return response.json();
         })
         .then(function(data) {
+            console.log(data);
             latitude = data.coord.lat;
             longitude = data.coord.lon;
+            console.log(latitude,longitude);
             addCity();
-            return latitude, longitude;
+            return requestTwo(latitude, longitude);
         })
 }
 
-
-
 function requestTwo(latitude,longitude) {
     var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + key
-    console.log(latitude);
-    console.log(longitude);
+    console.log(url2);
     fetch(url2)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            temp = data.current.temp;
+            humidity = data.current.humidity;
+            wind_speed = data.current.wind_speed;
+            uvi = data.current.uvi;
+            cityName.textContent = input;
+            cityTemp.textContent = "Temperature: " + temp;
+            cityHumid.textContent = "Humidity: " + humidity;
+            cityWind.textContent = "Wind Speed: " + wind_speed;
+            cityUV.textContent = "UV Index: " + uvi;
+        })
 }
 
 
